facafd289314805d44ed9a8e1d6dd47ceaee9dd3
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2023-03-22 17:48:21 +0100

    run_warc2text.sh: using filters from ParaCrawl if FILTER_DIR is specified, saving joblog for simple re-runnning failed tasks by GNU parallel

--- run_warc2text.sh
+++ run_warc2text.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-WARCS_DIR=$1
-OUTPUT_DIR=$2
-NJOBS=$3
+WARCS_DIR=$1  # directory with input WARC files
+OUTPUT_DIR=$2  # directory to write extracted texts and urls to
+NJOBS=$3  # number of parallel processes to fork
+FILTER_DIR=$4  # directory to take filter files from, if empty then no filtering is applied
 
-LOG_DIR=${OUTPUT_DIR}_logs
-BATCH_SIZE=100
+LOG_DIR=${OUTPUT_DIR%/}_logs # ${VAR%/} removes trailing slash if there is any
+BATCH_SIZE=100  # each run of warc2text processes BATCH_SIZE WARCs, too small batch will result in too many files generated which may lead to inodes exhaustion
 
 echo "Extracting texts from WARCs in ${WARCS_DIR} in ${NJOBS} parallel processes in batches of ${BATCH_SIZE} WARCs each. Saving texts to ${OUTPUT_DIR} and logs to ${LOG_DIR}."
 
 mkdir ${LOG_DIR} ${OUTPUT_DIR} || exit 1 
-find ${WARCS_DIR} -name "*.warc.gz" | sort | parallel --will-cite --halt now,fail=1 --eta --max-args ${BATCH_SIZE} --jobs=${NJOBS} "warc2text -o ${OUTPUT_DIR}/{#} {} &> ${LOG_DIR}/{#}.log"
 
-#TODO: 1. filter MT-generated texts 2. probably continue running even when some tasks fail, but make it simple to re-run failed tasks (--joblog and --retry-failed )
+if [[ -d $FILTER_DIR ]]; then
+    FILTERS="--tag-filters ${FILTER_DIR}/mt-filter-list.annotated  --url-filters ${FILTER_DIR}/url-filter-list.optimised"
+    echo "Using filters: ${FILTERS}"
+else
+    echo "WARNING: FILTER_DIR is not specified: using no filters!"	
+fi
+
+find ${WARCS_DIR} -name "*.warc.gz" | 
+sort | 
+parallel --will-cite --eta \
+    --joblog ${LOG_DIR}/joblog \
+    --max-args ${BATCH_SIZE} --jobs=${NJOBS} \
+    "warc2text -o ${OUTPUT_DIR}/{#} {} ${FILTERS} &> ${LOG_DIR}/{#}.log"
+
+JOBS_SUCCESS=`cat ${LOG_DIR}/joblog |cut -f 7|tail -n +2|python -c "import sys; l=list(map(bool,map(int,sys.stdin))); print(len(l)-sum(l),'/',len(l))"`
+echo "$JOBS_SUCCESS jobs finished successfully, see ${LOG_DIR}/joblog for details. Run the following command to rerun failed jobs if any:"
+echo "parallel --retry-failed --joblog ${LOG_DIR}/joblog"
+

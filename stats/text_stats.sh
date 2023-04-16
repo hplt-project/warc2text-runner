167ffed8272479bdfff6eb1755d6f074c597fddf
nvanva <nick.arefyev@gmail.com>
2023-04-16 17:00:40 +0200

    Scripts to generate text statistics per language.

--- stats/text_stats.sh
+++ stats/text_stats.sh
@@ -5,3 +5,4 @@ NJOBS=$3
 mkdir $STATS_DIR
 find $GZ_DIR -name 'url.gz'|parallel --joblog $STATS_DIR/doc_stats.log --eta --jobs=$NJOBS "echo {}|tr '\n' ' '; zcat {}|wc" > $STATS_DIR/doc_stats.csv
 find $GZ_DIR -name 'text.gz'|parallel --joblog $STATS_DIR/text_stats.log --eta --jobs=$NJOBS "echo {}|tr '\n' ' '; zcat {}|base64 -d|wc" > $STATS_DIR/text_stats.csv
+

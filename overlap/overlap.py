fde5b46291ca3e4c35b5a39d01b166671b3648e6
nvanva <nick.arefyev@gmail.com>
2023-05-05 15:25:35 +0200

    overlap.py: prints statistics in tab-separated format to stdout and in human-readable format to stderr

--- overlap/overlap.py
+++ overlap/overlap.py
@@ -5,13 +5,13 @@ import sys
 import fire
 
 
-def set_file_overlap(set1, file2, url_preprocess=lambda url: url.strip):
+def set_file_intersection(set1, file2, url_preprocess=lambda url: url.strip):
     """
     Returns intersection between lines if file2 and set1. Lines of file2 are preprocessed with url_preprocess.
     """
     with gzip.open(file2, 'rt', encoding='utf-8') if file2.endswith('gz') else open(file2, 'rt', encoding='utf-8') as inp:
-        overlap = set1.intersection((url_preprocess(s) for s in inp))
-    return overlap
+        intersection = set1.intersection((url_preprocess(s) for s in inp))
+    return intersection
 
 
 def read_portion1(iterator, mem_limit, batch_size=10**8, debug=False, url_preprocess=lambda url: url.strip):
@@ -51,7 +51,7 @@ def read_portion(iterator, mem_limit, batch_size=10**6, debug=False):
             return set1
 
 
-def overlap(file1, file2, mem_limit = 5*2**30, domain_level=False, debug=False):  # 1 GB memory limit
+def intersect(file1, file2, mem_limit=5 * 2 ** 30, domain_level=False, debug=False):  # 1 GB memory limit
     """
     Reads urls from file1 in batches such that the memory consumption does not significantly exceeds mem_limit.
     For each batch, reports the number of urls/domains read, the number of unique urls/domains among them,
@@ -61,16 +61,25 @@ def overlap(file1, file2, mem_limit = 5*2**30, domain_level=False, debug=False):
         url_preprocess = lambda url: url.strip().split('//')[-1].split('/')[0]
     else:
         url_preprocess = lambda url: url.strip()
-    overlap = set()
-    with gzip.open(file1, 'rt', encoding='utf-8') if file1.endswith('gz') else open(file1, 'rt', encoding='utf-8') as inp:
+
+    intersection = set()
+    part = -1
+    with gzip.open(file1, 'rt', encoding='utf-8') if file1.endswith('gz') else open(file1, 'rt',
+                                                                                    encoding='utf-8') as inp:
         while True:
             set1, l = read_portion1(inp, mem_limit, url_preprocess=url_preprocess, debug=debug)
+            part += 1
             if not set1: break
-            ss = set_file_overlap(set1, file2, url_preprocess=url_preprocess)
-            overlap.update(ss)
-            print( f'{len(ss)} lines from {file2} found in portion of {len(set1)}/{l} unique/total lines of {file1}')
+            ss = set_file_intersection(set1, file2, url_preprocess=url_preprocess)
+            intersection.update(ss)
+            print(f'{len(ss)} lines from {file2} found in portion of {len(set1)}/{l} unique/total lines of {file1}',
+                  file=sys.stderr)
+            print('lines', f'{file1} part{part}', '-', l, sep='\t')
+            print('unique_lines', f'{file1} part{part}', '-', len(set1), sep='\t')
+            print('intersection', f'{file1} part{part}', file2, len(ss), sep='\t')
             del set1
-    print('Overlap size:', len(overlap))
+    print('Overlap size:', len(intersection), file=sys.stderr)
+    print('intersection', file1, file2, len(intersection), sep='\t')
 
 
-fire.Fire(overlap)
+fire.Fire(intersect)

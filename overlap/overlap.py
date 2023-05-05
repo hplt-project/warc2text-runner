9f66c424afdc9c35d04fb528793092f1d8c1dd95
nvanva <nick.arefyev@gmail.com>
2023-05-05 14:46:51 +0200

    overlap.py: domain level overlap calculation

--- overlap/overlap.py
+++ overlap/overlap.py
@@ -5,29 +5,31 @@ import sys
 import fire
 
 
-def set_file_overlap(set1, file2):
+def set_file_overlap(set1, file2, url_preprocess=lambda url: url.strip):
+    """
+    Returns intersection between lines if file2 and set1. Lines of file2 are preprocessed with url_preprocess.
+    """
     with gzip.open(file2, 'rt', encoding='utf-8') if file2.endswith('gz') else open(file2, 'rt', encoding='utf-8') as inp:
-        overlap = set1.intersection((s.strip() for s in inp))
+        overlap = set1.intersection((url_preprocess(s) for s in inp))
     return overlap
 
 
-
-def read_portion1(iterator, mem_limit, batch_size=10**6, debug=False):
+def read_portion1(iterator, mem_limit, batch_size=10**6, debug=False, url_preprocess=lambda url: url.strip):
     """
     From iterator reads items into a set while there are items available and
-    the memory consumption of the process stays within mem_limit. 
+    the memory consumption of the process stays within mem_limit.
+    Preprocesses items with url_preprocess.
     Returns a set of items read.
     """
     set1, l = set(), 0
     while batch := tuple(islice(iterator, batch_size)):
         l += len(batch)
-        set1.update((s.strip() for s in batch))
+        set1.update((url_preprocess(s) for s in batch))
         mem = psutil.Process().memory_info().rss
         if debug: print(mem / 2**30, len(set1)/10**6, file=sys.stderr)
         if mem > mem_limit:
             return set1, l
     return set1, l
-
 
 
 def read_portion(iterator, mem_limit, batch_size=10**6, debug=False):
@@ -49,14 +51,22 @@ def read_portion(iterator, mem_limit, batch_size=10**6, debug=False):
             return set1
 
 
-
-def overlap(file1, file2, mem_limit = 8*2**30):  # 8 GB memory limit
+def overlap(file1, file2, mem_limit = 5*2**30, domain_level=False, debug=False):  # 1 GB memory limit
+    """
+    Reads urls from file1 in batches such that the memory consumption does not significantly exceeds mem_limit.
+    For each batch, reports the number of urls/domains read, the number of unique urls/domains among them,
+    and the number of unique urls/domains in file2 that are present in this batch.
+    """
+    if domain_level:
+        url_preprocess = lambda url: url.strip().split('//')[-1].split('/')[0]
+    else:
+        url_preprocess = lambda url: url.strip()
     overlap = set()
     with gzip.open(file1, 'rt', encoding='utf-8') if file1.endswith('gz') else open(file1, 'rt', encoding='utf-8') as inp:
         while True:
-            set1, l = read_portion1(inp, mem_limit)
+            set1, l = read_portion1(inp, mem_limit, url_preprocess=url_preprocess, debug=debug)
             if not set1: break
-            ss = set_file_overlap(set1, file2)
+            ss = set_file_overlap(set1, file2, url_preprocess=url_preprocess)
             overlap.update(ss)
             print( f'{len(ss)} lines from {file2} found in portion of {len(set1)}/{l} unique/total lines of {file1}')
             del set1

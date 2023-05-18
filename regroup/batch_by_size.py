b9dcc17e09be98cbe4d61a37fb6f04ee872c083f
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2023-05-18 22:31:02 +0200

    regroup: finished regrouping scripts

--- regroup/batch_by_size.py
+++ regroup/batch_by_size.py
@@ -1,21 +1,33 @@
 import pandas as pd
 import fire
+import os
+
 
 def batches_by_size(df, size=2**30):
     res = df.groupby(df['size'].cumsum() // size).agg({'size':'sum', 'path': ' '.join})
     return res
 
 
-def main(fsizes = 'sizes', fbatches = 'batches', size=2**30):
+def format_command(r, output_dir):
+    output_dir = os.path.join(output_dir, str(r.batch),r.lang)
+    url_paths = r.path.replace('text.gz','url.gz')
+    return f'mkdir -p {output_dir}; cat {r.path} > {output_dir}/text.gz; cat {url_paths} > {output_dir}/url.gz;'
+                
+
+def main(fsizes, output_dir, size=2**30):
     df = pd.read_csv(fsizes, sep='\t', header=None, names=['size','path'])
     df['lang'] = df.path.str.split('/').str[-2]
     bf = df.groupby('lang').apply(lambda df: batches_by_size(df, size))
     bf.index.rename({'size':'batch'}, inplace=True)
     bf = bf.reset_index()
 
-    bf.to_csv(fbatches, sep='\t')
+    bf.to_csv(os.path.join(output_dir, 'batches.tsv'), sep='\t', index=False)
 
-    print('\n'.join(bf.path))
+    commands = bf.apply(lambda r: format_command(r, output_dir), axis=1)
+    print('Lengths of commands:\n', commands.str.len().describe(), sep='')
+    with open(os.path.join(output_dir, 'tmp.sh'),'wt', encoding='utf-8') as outp:
+        outp.write('\n'.join(commands))
+
 
 
 fire.Fire(main)
--- regroup/run_regroup.sh
+++ regroup/run_regroup.sh
@@ -1,2 +1,22 @@
-find ./ -name 'text.gz' -print0 | du -b --files0-from=- > sizes
+#!/bin/bash
+
+DATASET_DIR=$1
+OUTPUT_DIR=$2
+
+BATCH_SIZE=1000000000
+NJOBS=240
+
+mkdir -p $OUTPUT_DIR
+
+# Find all text.gz files, calculate their sizes in bytes (-b) and save to sizes.tsv
+find $DATASET_DIR -name 'text.gz' -print0 | du -b --files0-from=- > ${OUTPUT_DIR}/sizes.tsv
+
+# Generate a script that concatenates text.gz files for the same langauge into new text.gz files of approximately BATCH_SIZE bytes;
+# if there are not enough texts for a languge, the resulting text.gz will be smaller; 
+# if some of the original text.gz files is larger, the resulting text.gz will be larger;
+# corresponding url.gz will also be concatenated;
+# the results will be saved into directories BATCH_ID/LANGUAGE, where BATCH_ID is batch number counted independently for each language.
+python batch_by_size.py ${OUTPUT_DIR}/sizes.tsv ${OUTPUT_DIR} ${BATCH_SIZE}
+# Run the generated script in parallel.
+cat ${OUTPUT_DIR}/tmp.sh | parallel --jobs ${NJOBS}  -N1 --pipe bash
 

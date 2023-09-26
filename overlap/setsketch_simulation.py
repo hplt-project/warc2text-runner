7754aa9b09b5b8ad997df8f32fae6227cbcea3ea
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2023-09-26 20:13:07 +0200

    setsketch simulations: generating larger sets

--- overlap/setsketch_simulation.py
+++ overlap/setsketch_simulation.py
@@ -2,7 +2,7 @@ from fire import Fire
 import sketch_ds
 import mmh3
 
-def run(step, cnt=10**11, m=2**18):
+def run(step, cnt=4*10**11, m=2**18):
     hll = sketch_ds.setsketch.CSetSketch(m)
     for c,i in enumerate(range(step, step*cnt+1, step)):
         o,_ = mmh3.hash64(str(i), seed=0, signed=False)

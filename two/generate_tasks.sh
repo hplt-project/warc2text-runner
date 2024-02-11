93bfc1bbf3784a75cac620375864e001d87004ef
hplt <hplt@mon003-cl3-mka-ova1.du.cesnet.cz>
2024-02-11 17:15:35 +0100

    shuffle WARCs for each crawl to make more balanced tasks

--- two/generate_tasks.sh
+++ two/generate_tasks.sh
@@ -3,4 +3,4 @@ BS=$2
 REMOTECONFIG="--sshlogin 3/mon3,3/mon4,3/mon6"
 #REMOTECONFIG="--sshlogin 5/nird-c,5/nird-d"
 
-cat $RUNDIR/crawls.paths | parallel --joblog $RUNDIR/joblog_generate_batches --eta --keep-order --linebuffer $REMOTECONFIG "module purge; module load parallel; cd {}; find ./ -name '*.warc*.gz' | parallel -I [] --seqreplace [#] -n $BS echo ~/hplt/two/code/warc2text-runner/two/run_warc2html_task.sh [#] ~/hplt/two/text/{/} {} []" |gzip >$RUNDIR/tasks.args.gz
+cat $RUNDIR/crawls.paths | parallel --joblog $RUNDIR/joblog_generate_batches --eta --keep-order --linebuffer $REMOTECONFIG "module purge; module load parallel; cd {}; find ./ -name '*.warc*.gz' | shuf | parallel -I [] --seqreplace [#] -n $BS echo ~/hplt/two/code/warc2text-runner/two/run_warc2html_task.sh [#] ~/hplt/two/text/{/} {} []" |gzip >$RUNDIR/tasks.args.gz

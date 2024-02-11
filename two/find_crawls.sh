839dfe6ecb07ba4c581b46a932a2ca91d4f46eb4
hplt <hplt@mon003-cl3-mka-ova1.du.cesnet.cz>
2024-02-11 17:16:42 +0100

    final versions of warc2html scripts

--- two/find_crawls.sh
+++ two/find_crawls.sh
@@ -1,12 +1,12 @@
-#RUNDIR=cesnet
-#REMOTECONFIG="--sshloginfile sshloginfile_cesnet"
-#PATTERNS="~/hplt/*/warc/*/*"
+RUNDIR=cesnet
+REMOTECONFIG="--sshloginfile sshloginfile_cesnet"
+PATTERNS="~/hplt/*/warc/*/*"
 
-RUNDIR=nirdl
-REMOTECONFIG=""
-PATTERNS="~/hplt/one/warc/ia/* ~/hplt/two/warc/archivebot/* ~/hplt/two/warc/cc/CC* ~/hplt/two/warc/cc/_SAMPLE/*"
+#RUNDIR=nirdl
+#REMOTECONFIG=""
+#PATTERNS="~/hplt/one/warc/ia/* ~/hplt/two/warc/archivebot/* ~/hplt/two/warc/cc/CC* ~/hplt/two/warc/cc/_SAMPLE/*"
 
-
+module purge; module load parallel; module unload OpenSSL
 mkdir -p $RUNDIR
 parallel $REMOTECONFIG --keep-order "eval echo {}" ::: $PATTERNS | tr ' ' '\n'  |tee $RUNDIR/crawls.paths
 parallel $REMOTECONFIG --keep-order "find {} -name '*.warc.gz'|wc -l|tr '\n' '\t'; du -sh {}" :::: $RUNDIR/crawls.paths |tee $RUNDIR/crawls.stat

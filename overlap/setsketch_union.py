ecd9735eddd4e0dc645f43d745efff5c092e6998
Nikolay Arefev <nikolare@hplt.nird.hplt-ns8112k.svc.cluster.local>
2024-02-11 17:28:02 +0100

    working on building setsketches

--- overlap/setsketch_union.py
+++ overlap/setsketch_union.py
@@ -2,12 +2,13 @@ import sketch_ds
 from pathlib import Path
 import fire
 
-def union(sketch_dir):
+def union(sketch_dir, outpath):
     ss_res = None
     for p1 in Path(sketch_dir).glob('*.setsketch'):
         ss = sketch_ds.setsketch.CSetSketch(str(p1)) 
         print('%e' % ss.report(), p1)
         ss_res = ss if ss_res is None else ss_res.union(ss)
+    ss_res.write(outpath)
     print('%e' % ss_res.report(), 'total size')
 
 fire.Fire(union)

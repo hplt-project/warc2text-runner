7acf426782900e960e10fde237be4c8e9b2bb698
nvanva <nick.arefyev@gmail.com>
2024-03-05 01:53:30 +0100

    traf.py: running Trafilatura

--- two/trafilatura/traf.py
+++ two/trafilatura/traf.py
@@ -9,21 +9,31 @@ import codecs
 def traf(instream, fast_mode):
     trafilatura_options = {"include_comments": False, "include_tables": False, "no_fallback": True} if fast_mode \
         else {}
-
+    unicode_error = False
+    cnt = 0
     while True:
         try:
             line = instream.readline()
-            if line == '':
-                break
+            if unicode_error:
+                unicode_error = False
+                continue  # recovering after UnicodeDecodeError: reading the rest of line and skipping it
+        except UnicodeDecodeError as e:
+            print(f'ERROR while reading line {cnt + 1}', e, file=sys.stderr)
+            unicode_error = True
+            continue
 
+        if line == '':
+            break
+        cnt += 1
+
+        try:
             d = json.loads(line.strip())
             html = d['h']
             text = trafilatura.extract(html, **trafilatura_options)
             print(json.dumps({'t': text}))
             # print(text)
-
-        except UnicodeDecodeError as e:
-            print(e, file=sys.stderr)
+        except Exception as e:
+            print(f'ERROR while reading line {cnt+1}', e, file=sys.stderr)
 
 
 def main(fpath='-', fast_mode=True, decoding_errors='ignore'):

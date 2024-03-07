296e3e7725a32d0c54865fae5efe44a37379516c
rggdmonk <32217886+rggdmonk@users.noreply.github.com>
2024-03-07 11:10:24 +0000

    Add init langid script

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Initial commit
+# Language identification for HPLT
+
+
+
+## Obstacles
+
+1) What is the best way to install and use fastText?
+
+    a. Via [building for python](https://github.com/facebookresearch/fastText?tab=readme-ov-file#building-fasttext-for-python) or via [building for CMake](https://github.com/facebookresearch/fastText?tab=readme-ov-file#building-fasttext-using-cmake)
+
+    b. Kenneth merged [the pull request for faster inference](https://github.com/facebookresearch/fastText/pull/1341). For what models/hardware configs is this faster? Need tests.
+
+    c. Kenneth tested on `nllblid218e` and  `lid.176.bin` models.
+
+2) What model/cleaning to use?
+
+    a. Laurie has `lid201-model.bin` model (from this [repo](https://github.com/laurieburchell/open-lid-dataset))
+
+    b. `lid201-model.bin` trained on super-clean data. See this scripts [here](https://github.com/laurieburchell/open-lid-dataset/tree/main/data_cleaning).
+
+    c. Laurie will release a new model soon and it also uses clean data.
+    https://github.com/laurieburchell/cs-lid-harder-than-you-think
+
+
+
+
+
+
+
+
---
+++ proto_langid.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+import sys
+import fasttext
+import fileinput
+import argparse
+import json
+
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+class FastTextLangId:
+    def __init__(self, model_path: str | Path):
+        """Initializes the FastText model."""
+        print(f"Loading model from: {model_path}")
+        self.model = fasttext.load_model(model_path)
+
+    def _preproccess_text(self, text: str) -> str:
+        """Preprocesses the text"""
+        # TODO: Ask Laurie about prepeprocessing
+        return text.replace("\n", " ").strip()
+
+    def _postprocess_prediction(self, prediction: tuple) -> str:
+        """Postprocesses the prediction."""
+        return prediction[0][0].split("__label__")[1] + "\n"
+
+    def predict_language_from_stdin(self) -> None:
+        """Reads line by line from standard input and writes the predicted language to standard output."""
+
+        with fileinput.input(files=("-",), encoding="utf-8") as f:
+            for fileinput_line in f:
+                prediction = self.model.predict(
+                    self._preproccess_text(fileinput_line), k=1
+                )
+                sys.stdout.write(self._postprocess_prediction(prediction))
+
+        return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Predict language using FastText model."
+    )
+    parser.add_argument(
+        "--model_path",
+        default="models/lid201-model.bin",
+        help="Path to the FastText model file",
+    )
+
+    parser.add_argument(
+        "--mode",
+        default="stdin",
+        choices=["stdin"],
+        help="Mode of input",
+    )
+
+    args = parser.parse_args()
+
+    print("Started processing")
+
+    loaded_model = FastTextLangId(args.model_path)
+
+    if args.mode == "stdin":
+        print(f"Reading from standard input! --mode is {args.mode}")
+        loaded_model.predict_language_from_stdin()
+
+    else:
+        raise NotImplementedError("Other input methods not implemented yet!")

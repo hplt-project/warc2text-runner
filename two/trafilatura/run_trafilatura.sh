cab060337deec9b7b872e17e620478b37cea6c9e
nvanva <nikolare@uio.no>
2024-03-12 16:39:36 +0200

    run_trafilatura.sh: tweaks in block size for parallel

--- two/trafilatura/run_trafilatura.sh
+++ two/trafilatura/run_trafilatura.sh
@@ -1,7 +1,11 @@
 FIN=$1
 OUTDIR=$2
 NJOBS=$3
-BLOCKSIZE=100M
+BLOCKSIZE=10M
+
+module purge
+module load LUMI/23.09 cray-python parallel/20231022
 
 mkdir -p $OUTDIR
-zstdcat $FIN |parallel --block $BLOCKSIZE -j $NJOBS --joblog ${OUTDIR}/joblog --pipe --keep-order  "python traf.py 2>${OUTDIR}/{#}.stderr"|zstd -o ${OUTDIR}/text.zst
+zstdcat $FIN |parallel --block $BLOCKSIZE -j $NJOBS --joblog ${OUTDIR}/joblog --pipe --keep-order  "python traf.py" | zstd > ${OUTDIR}/text.zst
+

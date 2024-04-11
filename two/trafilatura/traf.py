b559ee9268017f9abb9df0d47467368e78d93bdc
nvanva <nick.arefyev@gmail.com>
2024-04-12 00:10:44 +0200

    traf.py: disabling Trafilatura from falling back to the baseline method that poorly remove boilerplate for short pages; measuring extraction time

--- two/trafilatura/traf.py
+++ two/trafilatura/traf.py
@@ -7,12 +7,17 @@ import fire
 import zstandard
 import codecs
 import traceback
+from trafilatura.settings import use_config
+from timeit import default_timer as timer
 
-def traf(instream, fast_mode, decoding_errors):
-    trafilatura_options = {"include_comments": False, "include_tables": False, "no_fallback": True} if fast_mode \
-        else {}
+
+def traf(instream, fast_mode, decoding_errors, min_extracted_size=0):
+    trafilatura_options = {"include_comments": False, "include_tables": False, "no_fallback": fast_mode}
+    config = use_config()
+    config.set("DEFAULT", "MIN_EXTRACTED_SIZE", str(min_extracted_size))
 
     for byteline in instream:
+        st = timer()
         errors = []
 
         try:
@@ -27,24 +32,35 @@ def traf(instream, fast_mode, decoding_errors):
             try:
                 d = json.loads(line.strip())
                 html = d['h']
-                text = trafilatura.extract(html, **trafilatura_options)
+                text = trafilatura.extract(html, config=config, **trafilatura_options)
             except Exception as e:
                 errors.append(traceback.format_exc())
                 text = None
 
-        print(json.dumps({'t': text, 'e': errors}))
+        dur = timer() - st
+        print(json.dumps({'t': text, 'e': errors, 'dur': f'{dur:.1e}'}))
 
 
-def main(fpath='-', fast_mode=True, decoding_errors='ignore'):
+def main(fpath: str = '-', fast_mode: bool = False, decoding_errors: str = 'ignore', min_extracted_size: int = 0):
+    """
+    Extracts texts from HTMLs using Trafilatura library.
+    Reads jsonlines with "h" field containing HTMLs from stdin or file. Writes jsonlines to stdout containing text
+    extracted, any errors occurred while decoding from utf-8 or text extraction, document processing time.
+    In case of errors returns None in the 't' field instead of extracted text.
+
+    :param fpath: path to the input .zst or '-' to read from stdin
+    :param fast_mode:
+    :param decoding_errors: how to handle utf-8 decoding errors, see https://docs.python.org/3/library/functions.html#open for options;
+    the script will always try to decode in 'strict' mode to detect and report any errors, if decoding_errors!='strict'
+    then in case of errors will retry using the specified mode.
+    :param min_extracted_size: when Trafilatura cannot extract text of at least this length from HTML, it falls back
+    to a simple baseline extraction algorithm (that almost doesn't remove boilerplate and performs poorly according
+    to the evaluation in the whitepaper). Recommended to keep the default value of 0 to keep boilerplate removed for
+    short pages.
     """
 
-    :param fpath:
-    :param fast_mode:n
-    :param decoding_errors: how to handle utf-8 decoding errors, see https://docs.python.org/3/library/functions.html#open for options
-    :return:
-    """
     with sys.stdin.buffer if fpath == '-' else io.BufferedReader(zstandard.open(fpath, 'rb')) as inp:
-        traf(inp, fast_mode, decoding_errors)
+        traf(inp, fast_mode, decoding_errors, min_extracted_size)
 
 
 fire.Fire(main)

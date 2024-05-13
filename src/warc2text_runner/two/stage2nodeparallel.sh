a124c4e695d676876ae21bf593c67c54f79d06e3
nvanva <nick.arefyev@gmail.com>
2024-05-13 13:55:45 +0200

    setting 7h max processing time for 1 file of stage2, 99.9% should fit

--- src/warc2text_runner/two/stage2nodeparallel.sh
+++ src/warc2text_runner/two/stage2nodeparallel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 FPATHS=$1
 cat $FPATHS | parallel --eta --joblog $FPATHS.joblog -N 1 -j 100 \
-  "srun --quiet --nodes=1 --cpus-per-task=128 --account=project_465000498 --partition=standard  --time=2-00:00:00 stage2local.sh {} {//} 250 &>$FPATHS.{#}.out"
+  "srun --quiet --nodes=1 --cpus-per-task=128 --account=project_465000498 --partition=standard  --time=0-07:00:00 stage2local.sh {} {//} 250 &>$FPATHS.{#}.out"

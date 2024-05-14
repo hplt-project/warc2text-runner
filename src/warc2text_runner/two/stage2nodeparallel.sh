39340ebb9261e02f0b90265f6eef58c62f806cb8
nvanva <nick.arefyev@gmail.com>
2024-05-14 16:59:31 +0200

    working on stage2 with staging

--- src/warc2text_runner/two/stage2nodeparallel.sh
+++ src/warc2text_runner/two/stage2nodeparallel.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-FPATHS=$1
-cat $FPATHS | parallel --eta --joblog $FPATHS.joblog -N 1 -j 100 \
+NJOBS=$1
+FPATHS=$2
+cat $FPATHS | parallel --eta --joblog $FPATHS.joblog -N 1 -j $NJOBS \
   "srun --quiet --nodes=1 --cpus-per-task=128 --account=project_465000498 --partition=standard  --time=0-07:00:00 stage2local.sh {} {//} 250 &>$FPATHS.{#}.out"
--- src/warc2text_runner/two/stage2preplumic.sh
+++ src/warc2text_runner/two/stage2preplumic.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 module purge
-module load LUMI/23.09 partition/C cray-python/3.10.10 parallel/20231022
+module load LUMI/23.09 partition/C cray-python/3.10.10 parallel/20231022 lumio

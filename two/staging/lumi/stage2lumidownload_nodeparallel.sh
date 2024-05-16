9956f09323631261b2cf0e415acb5c23529897df
nvanva <nikolare@uio.no>
2024-05-16 20:16:13 +0300

    working on stage2 with staging

--- two/staging/lumi/stage2lumidownload_nodeparallel.sh
+++ two/staging/lumi/stage2lumidownload_nodeparallel.sh
@@ -6,5 +6,5 @@ FIFO=${DATACENTER}.fifo
 
 REMOTECONFIG="--sshlogin 1/uan01,1/uan02,1/uan04"
 
-cat $FPATHS | parallel --line-buffer $REMOTECONFIG --eta -N 48 "cd ~/hplt/two/code/warc2text-runner/two/staging/lumi; ./stage2lumidownload_local.sh $DATACENTER $LPATH {}" | tee -a ${DATACENTER}.download.log >>$FIFO
+cat $FPATHS | parallel --joblog ${DATACENTER}.download.joblog --line-buffer $REMOTECONFIG --eta -N 48 "cd ~/hplt/two/code/warc2text-runner/two/staging/lumi; ./stage2lumidownload_local.sh $DATACENTER $LPATH {}" | tee -a ${DATACENTER}.download.log >>$FIFO
 

d4bb1988b22416a3781ffe7b6a782a11be1b6f64
nvanva <nikolare@uio.no>
2024-05-16 19:17:43 +0300

    working on stage2 with staging

--- two/staging/stage2wstaging1.sh
+++ two/staging/stage2wstaging1.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-RPATH=$1
-LPATH=$2
-MAXNODES=$3
+DATACENTER=$1
+FPATHS=$2
+LPATH=$3
+MAXNODES=$4
 
-NAME=$(basename $RPATH)
+NAME=$DATACENTER
 FIFO=${NAME}.fifo
 
 mkfifo $FIFO  # create named pipe (queue); works well only within 1 node!
 
 set -euo pipefail
 echo Calculating the number of files to process for the progress bar. Please wait ...
-NFILES=`rclone ls $RPATH --include html.zst|wc -l`
-echo In total $NFILES should be downloaded. Please start downloading.
+NFILES=`grep html.zst $FPATHS | wc -l`
+echo $NFILES html.zst files are expected to be downloaded and processed in total. Please start downloading.
 
 # this will select messages about html.zst files downloaded from the pipe, form a local path to html.zst 
 # and process with stage2nodeparallel
@@ -19,7 +20,7 @@ cat $FIFO | tee -a ${NAME}.download.log | \
         # save rclone logs to a file; a pipe cannot store anything, only transfer ; append to the existing logs in order to make this script restartable
 	grep  --line-buffered -E "html.zst.*Copied" | \
 	# select messages about html.zst downloaded, line buffering to process immediately 
-	sed --unbuffered -r 's!^.*: (.*.zst).*$!'${LPATH}'/'${NAME}'/\1!' | \
+	sed --unbuffered -r 's!^.*: (.*.zst).*$!'${LPATH}'/\1!' | \
 	# extract local path to downloaded html.zst, --unbuffered to process immeditely 
 	parallel --eta --total-jobs $NFILES --joblog $NAME.stage2.joblog -N 1 -j $MAXNODES \
 	  "srun --quiet --nodes=1 --cpus-per-task=128 --account=project_465000498 --partition=standard  --time=0-07:00:00 stage2local.sh {} {//} 250 &>$NAME.{#}.stage2.out && rm {}"

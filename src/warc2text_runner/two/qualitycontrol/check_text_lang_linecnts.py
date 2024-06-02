67f708dad8c5943701d591751a6e7a787e3e65bc
nvanva <nikolare@uio.no>
2024-06-03 02:27:09 +0300

    sacct logs for all stage2

--- src/warc2text_runner/two/qualitycontrol/check_text_lang_linecnts.py
+++ src/warc2text_runner/two/qualitycontrol/check_text_lang_linecnts.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import fire
 
-df=pd.read_csv('text_lang_linecnts.tsv', sep='\s+', header=None, names=['dir']+[f'{f}-{c}' for f in ('metadata','text','lang') for c in ('lines','words','bytes')])
-print('\n'.join(df[df['metadata-lines'] != df['text-lines']]['dir']))
-print('\n'.join(df[df['metadata-lines'] != df['lang-lines']]['dir']))
+def check_tsv(tsv):
+    df=pd.read_csv(tsv, sep='\s+', header=None, names=['dir']+[f'{f}-{c}' for f in ('metadata','text','lang') for c in ('lines','words','bytes')])
+    print('metadata-text lines mismatch:\n', '\n'.join(df[df['metadata-lines'] != df['text-lines']]['dir']))
+    print('metadata-lang lines mismatch:\n','\n'.join(df[df['metadata-lines'] != df['lang-lines']]['dir']))
+
+
+fire.Fire(check_tsv)

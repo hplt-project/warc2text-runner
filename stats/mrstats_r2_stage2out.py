6f19636dcfd8f6198adfbb4ca5b4adbf7ad411d4
nvanva <nick.arefyev@gmail.com>
2024-10-08 20:22:03 +0200

    statistics scripts for release2 stage2out and other stages

--- stats/mrstats_r2_stage2out.py
+++ stats/mrstats_r2_stage2out.py
@@ -1,20 +1,16 @@
 from fire import Fire
-import zstandard
-import pandas as pd
-import sys
-
-from stats.mrstats_base import MRStatsR2
+from stats.mrstats_r2 import MRStatsR2
 
 
 class MRStatsR2S2(MRStatsR2):
     def __init__(self, collection):
+        super().__init__()
         self.collection = collection
 
 
     def _build_index(self, df):
         df['index'] = self.collection + ','  # given constant value
         df['index'] += df.lang.str[0].fillna('null')
-
 
 
 Fire(MRStatsR2S2)

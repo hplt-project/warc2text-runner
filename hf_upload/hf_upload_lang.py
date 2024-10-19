f317f861238c77cab47aa26a0417cac31cac66b8
nvanva <nick.arefyev@gmail.com>
2024-10-19 14:29:18 +0200

    removed breakpoints

--- hf_upload/hf_upload_lang.py
+++ hf_upload/hf_upload_lang.py
@@ -8,7 +8,6 @@ def upload(lang_dir, remote, cache_dir='/nird/projects/NS8112K/two/cache'):
     part = p.name
     files = [str(f) for f in p.glob("*.zst")]
     print(f'Loading part {part} from {p}: {len(files)} files. Cache dir: {cache_dir}')
-    import pdb; pdb.set_trace()
     ds = load_dataset('json', data_files=files, cache_dir=cache_dir)
     print(f'Pushing part {part} from {p} to {remote}')
     ds.push_to_hub(remote, part)

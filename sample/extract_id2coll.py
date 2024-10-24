551a4cebab348317c1921980a28465ac9c39d786
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2024-10-25 01:52:24 +0200

    extract_id2coll.py for release2_annotation

---
+++ sample/extract_id2coll.py
@@ -0,0 +1,16 @@
+from tqdm import tqdm
+import pandas as pd
+from pathlib import Path
+from fire import Fire
+
+
+def extract_id2c():
+    dfs = []
+    for p in tqdm(list(Path('./per_lang_group_1K').glob('*/*zst'))): 
+        df1 = pd.read_json(p, orient='records', lines=True)
+        dfs.append(df1)
+    df1 = pd.concat(dfs, ignore_index=True)
+    df1[['id','c']].to_csv('per_lang_group_1K_id2c.tsv', index=False)
+    
+
+Fire(extract_id2c)

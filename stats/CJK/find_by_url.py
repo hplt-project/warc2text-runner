8619d47d9a879909914e60261205a9999f8e8261
nvanva <nick.arefyev@gmail.com>
2024-10-27 01:06:12 +0200

    from stats/marstats*.py unifying_iterator is extracted for reuse

--- stats/CJK/find_by_url.py
+++ stats/CJK/find_by_url.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from warc2text_runner.utils.unifying_iterator import batch_iterator
+from warc2text_runner.utils.unifying_iterator import batch_iterator, _release2_iterator
 import sys
 from fire import Fire
 
@@ -7,8 +7,8 @@ from fire import Fire
 def retrieve(fqueries, file, *files):
     inps = [file] + list(files)
     qdf = pd.read_csv(fqueries, sep='\t', names=['url','text'])
-    import pdb; pdb.set_trace()
-    for df in batch_iterator('r2', inps, batch_size=10**5):
+    # import pdb; pdb.set_trace()
+    for df in _release2_iterator(inps, batch_size=10**5):
         mdf = df.merge(qdf, on='url', how='inner')    
         if len(mdf) > 0:
             mdf.to_csv(sys.stdout)     

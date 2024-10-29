4ebddd7cee85d661e7e0918937291f47106c1324
nvanva <nick.arefyev@gmail.com>
2024-10-29 22:53:35 +0100

    hf_upload: fixing conversion errors by providing column type definitions

--- hf_upload/test_conversion_fail1.sh
+++ hf_upload/test_conversion_fail1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #for lang in kon_Latn; do
-for lang in bam_Latn uij_Arab; do
+for lang in bam_Latn uig_Arab; do
     wget -P ${lang} https://data.hplt-project.org/two/cleaned/${lang}/1.jsonl.zst
     python hf_upload_lang.py ${lang} nikolare/HPLT2.0_cleaned ./cache
 done

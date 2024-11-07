b717a593445790fdfef7c3d4ba24cf010bd05c0a
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2024-11-07 21:50:27 +0100

    unifying_iterator: encoding argument

--- src/warc2text_runner/utils/unifying_iterator.py
+++ src/warc2text_runner/utils/unifying_iterator.py
@@ -2,9 +2,9 @@ import base64
 import pandas as pd
 
 
-def _release2_iterator(inps, batch_size):
+def _release2_iterator(inps, batch_size, encoding_errors):
     readers = [
-        pd.read_json(inp, orient='records', lines=True, chunksize=batch_size)
+        pd.read_json(inp, orient='records', lines=True, chunksize=batch_size, encoding_errors=encoding_errors)
         for inp in inps]
 #        import pdb; pdb.set_trace()
     for dfs in zip(*readers):
@@ -16,9 +16,9 @@ def _release2_iterator(inps, batch_size):
         yield df
 
 
-def _release1_warc2textout_iterator(inps, batch_size):
+def _release1_warc2textout_iterator(inps, batch_size, encoding_errors):
     assert len(inps) == 1, 'Specify only the file with base64-encoded texts! '
-    reader = pd.read_csv(inps[0], sep='\t', header=None, chunksize=batch_size, names=['text'])
+    reader = pd.read_csv(inps[0], sep='\t', header=None, chunksize=batch_size, names=['text'], encoding_errors=encoding_errors)
     for df in reader:
         df.text = df.text.apply(lambda b: base64.b64decode(b).decode('utf-8'))
         yield df
@@ -30,8 +30,8 @@ version2impl = {
 }
 
 
-def batch_iterator(data_version, inp, batch_size=10**5):
+def batch_iterator(data_version, inp, batch_size=10**5, encoding_errors):
     f = version2impl.get(data_version)
     if f is None:
         raise ValueError(f'Unsupported dataset version {data_version}, select among {version2impl.keys()}')
-    f(inp, batch_size)
+    f(inp, batch_size, encoding_errorrs)

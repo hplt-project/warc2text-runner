0e0c332a386b75f513f07b7a2a064bcd62dfa0cf
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2024-11-12 12:45:20 +0100

    Updated stats for the stage2 outputs of release 2; now the number of characters in empty documents is counted as 0, note as len('None')=4

--- stats/text_stats_mr.sh
+++ stats/text_stats_mr.sh
@@ -1,8 +1,8 @@
+# Statistics for Release 2, just after stage2 (WP2)
 GZ_DIR=$1
 STATS_DIR=$2
 NJOBS=$3
 
 mkdir $STATS_DIR
-find $GZ_DIR -name 'text.*'|parallel --joblog $STATS_DIR/text_stats.log --eta --jobs=$NJOBS 'python mrstats_r2.py map {} {//}/lang.zst --data_version=r2 --collection="$(basename $(dirname {//}))" ' > $STATS_DIR/text_stats.csv
-#python reduce_stats.py $STATS_DIR
-#python plot_lang_stats.py $STATS_DIR
+find $GZ_DIR -name 'text.*'|parallel --joblog $STATS_DIR/text_stats.log --eta --jobs=$NJOBS 'python mrstats.py map {} {//}/lang.zst --collection="$(basename $(dirname {//}))" ' > $STATS_DIR/text_stats.csv
+python mrstats.py reduce $STATS_DIR

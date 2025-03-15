f9969c015ac6edd50e902c5ed701e65a4e72101f
nvanva <nikolare@uio.no>
2025-03-16 00:42:46 +0200

    stage2: specifying outdir explicitly

--- src/warc2text_runner/two/stage2local_batch.sh
+++ src/warc2text_runner/two/stage2local_batch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+BASEOUTDIR=$1
+
 rc=0
-for x in $@; do 
-    echo Processing $x 1>&2  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
-    time stage2local.sh $x $(dirname $x) 250 || (rc=$? && echo "ERROR while processing $x" 1>&2)  # in bash && and || have the same priority
+for x in ${@:2}; do 
+    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r 's!.*(/[^/]+/[^/]+/)[^/]+!\1!'`
+    echo "Processing $x, writing to ${OUTDIR}" 1>&2  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
+    time stage2local.sh $x ${OUTDIR} 250 || (rc=$? && echo "ERROR while processing $x" 1>&2)  # in bash && and || have the same priority
     echo "Current error code:" $rc 1>&2
     
 done
--- src/warc2text_runner/two/stage2nodeparallel_batched.sh
+++ src/warc2text_runner/two/stage2nodeparallel_batched.sh
@@ -2,6 +2,7 @@
 NJOBS=$1
 FPATHS=$2
 BATCH_GB=$3
+OUTDIR=$4
 
 cat $FPATHS | python -m warc2text_runner.two.batch_htmls $BATCH_GB | parallel --eta --joblog $FPATHS.joblog -N 1 -j $NJOBS \
-  "srun --quiet --nodes=1 --cpus-per-task=128 --account=project_465000498 --partition=standard  --time=0-48:00:00 stage2local_batch.sh {} &>$FPATHS.{#}.out"
+  "srun --quiet --nodes=1 --cpus-per-task=128 --time=0-48:00:00 stage2local_batch.sh $OUTDIR {} &>$FPATHS.{#}.out"

269df6c6eae0a6d33cb600253c78519b2a930efa
nvanva <nikolare@uio.no>
2025-03-16 02:01:00 +0200

    cputime for stage2 on LUMI

---
+++ three/cputime2.sh
@@ -0,0 +1,4 @@
+LOGDIR=sample_twoenv_lumio
+echo "real time from time, hours:"
+grep real $LOGDIR/*out >realtime_time.tsv
+python -c "import pandas as pd; print(pd.to_timedelta(pd.read_csv('realtime_time.tsv', header=None, sep='\t')[1]).sum().total_seconds() / 3600)"

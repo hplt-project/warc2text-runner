d456c2dbc9a9681585eee7bf6eed47243279d999
nvanva <ezhick179@gmail.com>
2025-04-02 12:45:53 +0200

    stage2: Trafilatura hyperparameters selected

--- src/warc2text_runner/stage2/trafilatura/traf.py
+++ src/warc2text_runner/stage2/trafilatura/traf.py
@@ -26,11 +26,12 @@ def time_limit(seconds):
         signal.setitimer(signal.ITIMER_REAL, 0)
 
 
-def traf(instream, fast_mode, decoding_errors, min_extracted_size=0, timelimit_perdoc=None, matcher=None):
-    trafilatura_text_options = {"include_comments": False, "include_tables": False, "no_fallback": fast_mode}
-    trafilatura_xml_options = {"include_comments": True, "include_tables": True, "no_fallback": fast_mode,
-                               "with_metadata": False, "include_formatting": False}
-
+def traf(instream, decoding_errors, timelimit_perdoc=None, matcher=None):
+    trafilatura_text_options = {"include_comments": True, "include_tables": False,
+                                "no_fallback": False, "favor_precision": True, "favor_recall": False}
+    trafilatura_xml_options = trafilatura_text_options | {"include_comments": True, "include_tables": True,
+                                                          "with_metadata": False, "include_formatting": True}
+    min_extracted_size = 250
     config = use_config()
     config.set("DEFAULT", "MIN_EXTRACTED_SIZE", str(min_extracted_size))
 
@@ -78,28 +79,22 @@ def traf(instream, fast_mode, decoding_errors, min_extracted_size=0, timelimit_p
         print(json.dumps(res))
 
 
-def main(fpath: str = '-', fast_mode: bool = False, decoding_errors: str = 'ignore', min_extracted_size: int = 0,
-         timelimit_perdoc: float = None):
+def main(fpath: str = '-', decoding_errors: str = 'ignore', timelimit_perdoc: float = None):
     """
     Extracts texts from HTMLs using Trafilatura library.
     Reads jsonlines with "h" field containing HTMLs from stdin or file. Writes jsonlines to stdout containing text
-    extracted, any errors occurred while decoding from utf-8 or text extraction, document processing time.
-    In case of errors returns None in the 't' field instead of extracted text.
+    extracted, any errors occurred while decoding from utf-8 or text extraction, additional metadata.
+    In case of unrecoverable errors returns None in the 't' field instead of the extracted text.
 
     :param fpath: path to the input .zst or '-' to read from stdin
-    :param fast_mode:
     :param decoding_errors: how to handle utf-8 decoding errors, see https://docs.python.org/3/library/functions.html#open for options;
     the script will always try to decode in 'strict' mode to detect and report any errors, if decoding_errors!='strict'
     then in case of errors will retry using the specified mode.
-    :param min_extracted_size: when Trafilatura cannot extract text of at least this length from HTML, it falls back
-    to a simple baseline extraction algorithm (that almost doesn't remove boilerplate and performs poorly according
-    to the evaluation in the whitepaper). Recommended to keep the default value of 0 to keep boilerplate removed for
-    short pages.
     :param timelimit_perdoc: sets maximum time (in seconds) for processing 1 document with Trafilatura
     """
     matcher = TagFilter()
     with sys.stdin.buffer if fpath == '-' else io.BufferedReader(zstandard.open(fpath, 'rb')) as inp:
-        traf(inp, fast_mode, decoding_errors, min_extracted_size, timelimit_perdoc, matcher)
+        traf(inp, decoding_errors, timelimit_perdoc, matcher)
 
 
 if __name__ == '__main__':

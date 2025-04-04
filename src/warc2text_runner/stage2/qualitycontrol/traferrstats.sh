17923d7bcf2a60a3d630e42333a2e99333f9f3f5
nvanva <ezhick179@gmail.com>
2025-04-04 22:59:15 +0200

    stage2: traferrstat.sh moved to src

--- src/warc2text_runner/stage2/qualitycontrol/traferrstats.sh
+++ src/warc2text_runner/stage2/qualitycontrol/traferrstats.sh
@@ -3,5 +3,5 @@ STATS_DIR=$2
 NJOBS=$3
 
 mkdir $STATS_DIR
-find $GZ_DIR -name 'text.*'|parallel --joblog $STATS_DIR/text_stats.log --eta --jobs=$NJOBS 'python traferrstats.py map {} {//}/lang.zst --collection="$(basename $(dirname {//}))" ' > $STATS_DIR/text_stats.csv
+find $GZ_DIR -name 'text.*'|parallel --joblog $STATS_DIR/text_stats.log --eta --jobs=$NJOBS 'python -m warc2text_runner.stage2.qualitycontrol.traferrstats map {} {//}/lang.zst --collection="$(basename $(dirname {//}))" ' > $STATS_DIR/text_stats.csv
 python -m warc2text_runner.stage2.qualitycontrol.traferrstats reduce $STATS_DIR

7dcd3f4b8ebdaf94648bcf08937f5256789ad081
nvanva <ezhick179@gmail.com>
2025-04-15 22:40:10 +0200

    stage2: stage2stagelumio.sh introduced for staging data from LUMIO

--- src/warc2text_runner/stage2/stage2stagelumio.sh
+++ src/warc2text_runner/stage2/stage2stagelumio.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-BASEOUTDIR=$1
+set -euo pipefail
 
-XX=`echo ${@:2} | sed 's/lumio://g'`
+BASEOUTDIR=$1  # directory to download files to, remote paths will be preserved inside this directory
+XX="${@:2}"  # html.zst files to download
+
 YY=`echo $XX | sed 's/html.zst/metadata.zst/g'`
 ZZ=`echo $XX | sed 's/html.zst/robotstxt.warc.gz/g'`
 
-echo $XX $YY $ZZ|tr ' ' '\n' | rclone copy -v lumio: $BASEOUTDIR --include-from="-" --transfers=64 --multi-thread-streams=0 --checkers=1
+# download all files from the batch with one rclone call; since we process 100-300 batches in parallel,
+# to reduce the number of parallel tasks we run only 1 checker for md5sum of the downloaded files and no
+# multi-thread downloads (on LUMI multi-thread downloads from lumio seems to be slower anyway)
+echo $XX $YY $ZZ | sed 's@lumio:@@g' | tr ' ' '\n' | \
+  rclone copy -v lumio: $BASEOUTDIR --files-from="-" --transfers=32 --multi-thread-streams=0 --checkers=1 --no-traverse
+ls `echo $XX $YY $ZZ | sed "s@lumio:@${BASEOUTDIR}/@g"` >/dev/null  # if any file is absent, return non-zero error code

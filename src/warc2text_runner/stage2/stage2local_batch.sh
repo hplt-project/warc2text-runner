9f89e7a44f9ac25e26275cb61e0e84ecb24cbc17
nvanva <ezhick179@gmail.com>
2025-04-17 01:40:52 +0200

    stage2: minor fixes

--- src/warc2text_runner/stage2/stage2local_batch.sh
+++ src/warc2text_runner/stage2/stage2local_batch.sh
@@ -10,6 +10,12 @@ for x in ${@:2}; do
     echo "Processing $x, writing to ${OUTDIR}" 1>&2  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
     time stage2local.sh $x ${OUTDIR} || { rc="$?"; echo "ERROR: stage2local.sh exited with code $rc while processing $x" 1>&2; }  # in bash && and || have the same priority
     echo "stage2local_batch.sh planned exit code:" $rc 1>&2
-    
 done
+
+if [ "$rc" -eq 0 ]; then
+  echo "stage2local_batch.sh: all files processed without errors"
+else
+  echo "stage2local_batch.sh: all files processed, errors occurred while processing"
+fi
+
 exit $rc

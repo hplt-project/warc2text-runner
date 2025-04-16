922d5441b81f006b9669ca357d235b87e3cda497
nvanva <ezhick179@gmail.com>
2025-04-17 00:02:53 +0200

    stage2: removed staging, streaming from lumio with s3cmd instead of rclone seems to solve the EOF crashes

--- src/warc2text_runner/stage2/stage2local.sh
+++ src/warc2text_runner/stage2/stage2local.sh
@@ -16,12 +16,20 @@ echo Running lid in $NJOBS_LID and trafilatura in $NJOBS_TRAF processes
 set -euo pipefail
 
 mkdir -p $OUTDIR
+
+S3FIN=`echo $FIN | sed 's@lumio:@s3://@'`
+s3cmd get `echo $FIN | sed 's@html.zst@metadata.zst@'` ${OUTDIR}/
+
 # --keep-order guarantees that GNU Parallel will collect stdout from tasks and print it to its stdout in the order
 # aligned with the order of input lines
 # --block issues one task per block of input lines of roughly this size, but without breaking lines;
 # making it too small will increase extra costs on script initialization (e.g. weights loading for langid),
 # making it too large will require buffering too much outputs in parallel due to --keep-order requirement.
-rclone cat $FIN | zstdcat  \
+
+# rclone occasionally crashes with EOF error when streaming some files from lumio (<1% for bs=10, ~50% for bs=1000)...
+# s3cmd shows warnings about EOF for these files, but retries with success
+#rclone cat $FIN | zstdcat  \
+s3cmd get $S3FIN - | zstdcat  \
     | parallel --halt now,fail=1 --block $BLOCKSIZE_TRAF -j $NJOBS_TRAF --pipe --keep-order  \
         "python -m warc2text_runner.stage2.trafilatura.traf --timelimit_perdoc ${TRAF_TIMEOUT}" | tee >(zstd > ${OUTDIR}/text.zst) \
     | parallel --halt now,fail=1 --block $BLOCKSIZE_LID -j $NJOBS_LID --pipe --keep-order \
@@ -31,14 +39,14 @@ rclone cat $FIN | zstdcat  \
 C=`paste <(zstdcat ${OUTDIR}/text.zst|wc -l) <(zstdcat ${OUTDIR}/lang.zst|wc -l) <(zstdcat ${OUTDIR}/metadata.zst|wc -l)`
 read t l m  <<< "$C"
 if [ "$t" != "$m" ]; then
-  echo "Number of lines mismatch: $t in ${OUTDIR}/text.zst and $m in ${OUTDIR}/metadata.zst"
+  echo "ERROR: Number of lines mismatch: $t in ${OUTDIR}/text.zst and $m in ${OUTDIR}/metadata.zst"
   exit 1
 fi
 
 if [ "$l" != "$m" ]; then
-  echo "Number of lines mismatch: $l in ${OUTDIR}/lang.zst and $m in ${OUTDIR}/metadata.zst"
+  echo "ERROR: Number of lines mismatch: $l in ${OUTDIR}/lang.zst and $m in ${OUTDIR}/metadata.zst"
   exit 1
 fi
 
 echo $t $l $m >${OUTDIR}/.done
-rm $FIN
+#rm $FIN
--- src/warc2text_runner/stage2/stage2local_batch.sh
+++ src/warc2text_runner/stage2/stage2local_batch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 BASEOUTDIR=$1
 
+echo "stage2local_batch.sh: processing ${@:2}, writing to $BASEOUTDIR"
 rc=0
 for x in ${@:2}; do 
-#    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r 's!.*(/[^/]+/[^/]+/)[^/]+!\1!'`
-    OUTDIR=$(dirname $x)
+#    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r 's!.*(/[^/]+/[^/]+/)[^/]+!\1!'`  # BASEOUTDIR/crawl/batch_id/
+#    OUTDIR=$(dirname $x)
+    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r "s@.*lumio:(.*)/html.zst@${OUTDIR}/\1@"`
     echo "Processing $x, writing to ${OUTDIR}" 1>&2  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
-    time stage2local.sh $x ${OUTDIR} || { rc="$?"; echo "ERROR while processing $x" 1>&2; }  # in bash && and || have the same priority
-    echo "Current error code:" $rc 1>&2
+    time stage2local.sh $x ${OUTDIR} || { rc="$?"; echo "ERROR: stage2local.sh exited with code $rc while processing $x" 1>&2; }  # in bash && and || have the same priority
+    echo "stage2local_batch.sh planned exit code:" $rc 1>&2
     
 done
 exit $rc

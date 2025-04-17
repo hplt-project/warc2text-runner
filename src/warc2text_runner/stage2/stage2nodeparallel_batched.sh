2391c995c76e75bbaead2535cdd66e4d54f84462
nvanva <ezhick179@gmail.com>
2025-04-17 12:28:44 +0200

    stage2: putting all logs to a new directory with the start datetime in the name

--- src/warc2text_runner/stage2/stage2nodeparallel_batched.sh
+++ src/warc2text_runner/stage2/stage2nodeparallel_batched.sh
@@ -4,6 +4,9 @@ FPATHS=$2
 BATCH_GB=$3
 OUTDIR=$4
 
+LOGDIR=logs_$(date +%Y-%m-%d-%H-%M-%S)
+mkdir $LOGDIR
+
 filter_done() {
   while read -r line; do
     fdone=`echo $line | sed -r "s@.*lumio:(.*)/html.zst@${OUTDIR}/\1/.done@"`
@@ -15,5 +18,5 @@ filter_done() {
   done
 }
 
-cat $FPATHS | filter_done | python -m warc2text_runner.stage2.batch_htmls $BATCH_GB | parallel --eta --joblog $FPATHS.joblog -N 1 -j $NJOBS \
-  "srun --quiet --nodes=1 --cpus-per-task=128 --time=0-48:00:00 stage2local_batch.sh $OUTDIR {} &>$FPATHS.{#}.out"
+cat $FPATHS | filter_done | python -m warc2text_runner.stage2.batch_htmls $BATCH_GB | parallel --eta --joblog $LOGDIR/joblog -N 1 -j $NJOBS \
+  "srun --quiet --nodes=1 --cpus-per-task=128 --time=0-48:00:00 stage2local_batch.sh $OUTDIR {} &>$LOGDIR/{#}.out"

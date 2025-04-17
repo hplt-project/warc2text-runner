1595d354cd88fb598fe94cbcf09dee1738e85a58
nvanva <ezhick179@gmail.com>
2025-04-17 13:14:00 +0200

    stage2: better batching method

---
+++ src/warc2text_runner/stage2/batch_htmls_prtpy.py
@@ -0,0 +1,18 @@
+from fire import Fire
+import sys
+import re
+import prtpy
+
+def batch_htmls(max_gb=1000):
+    f2s = {}
+    for l in sys.stdin:
+        sz,l = re.split(r'\s+',l.strip())
+        f2s[l] = float(sz)
+
+    res = prtpy.pack(algorithm=prtpy.packing.bin_completion, binsize=max_gb, items=f2s)
+    print('\n'.join(' '.join(r) for r in res))
+    total = sum(f2s.values()) / 2 ** 30
+    print(f'{len(f2s)} files of total size {total} GB packed into {len(res)} bins of max size {max_gb} GB.', file=sys.stderr)
+
+Fire(batch_htmls)
+

11c8c4944a927d5eb6285b7ca55c626e57e585ec
nvanva <ezhick179@gmail.com>
2025-04-17 15:09:13 +0200

    stage2: minor changes

--- src/warc2text_runner/stage2/batch_htmls_prtpy.py
+++ src/warc2text_runner/stage2/batch_htmls_prtpy.py
@@ -9,10 +9,11 @@ def batch_htmls(max_gb=1000):
         sz,l = re.split(r'\s+',l.strip())
         f2s[l] = float(sz)
 
-    res = prtpy.pack(algorithm=prtpy.packing.first_fit_decreasing, binsize=max_gb*2**30, items=f2s)
+    algo = prtpy.packing.first_fit_decreasing
+    res = prtpy.pack(algorithm=algo, binsize=max_gb*2**30, items=f2s)
     print('\n'.join(' '.join(r) for r in res))
     total = sum(f2s.values()) / 2 ** 30
-    print(f'{len(f2s)} files of total size {total} GB packed into {len(res)} bins of max size {max_gb} GB.', file=sys.stderr)
+    print(f'{len(f2s)} files of total size {total} GB packed into {len(res)} bins of max size {max_gb} GB with {algo}.', file=sys.stderr)
 
 Fire(batch_htmls)
 

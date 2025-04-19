ec3ab8f30734ff7e52cf5ef41fd7a50213cf9b0a
nvanva <ezhick179@gmail.com>
2025-04-19 15:07:54 +0200

    stage2: stage2local.sh refactored - split into preprocessing, processing, postprocessing functions, time measured separately; supporting both local and lumio: paths

--- src/warc2text_runner/stage2/stage2local.sh
+++ src/warc2text_runner/stage2/stage2local.sh
@@ -1,52 +1,69 @@
 #!/usr/bin/bash
 FIN=$1
 OUTDIR=$2
-NJOBS=`nproc --all`
-
-BLOCKSIZE_TRAF=30M  #  more parallel processes than for lid require smaller blocks;
-TRAF_TIMEOUT=10  # timeout 10s, increased from 0.5s to compensate for adding xml extraction for the 3rd iteration and hopefully get more long good texts
-
-BLOCKSIZE_LID=100M  # 0.3s-0.5s to load model, 4.4s to FastText.predict for 10k lines, 28 MB (not random sample!)
-
-NJOBS=$(($NJOBS - 8))  # leave some threads for rclone, zstdcat, zstd steps in the pipeline
-NJOBS_LID=$(($NJOBS/10 + 1))
-NJOBS_TRAF=$(($NJOBS - $NJOBS_LID))
-
 set -euo pipefail
-
 mkdir -p $OUTDIR
 
-S3FIN=`echo $FIN | sed 's@lumio:@s3://@'`
-s3cmd get `echo $S3FIN | sed 's@html.zst@metadata.zst@'` ${OUTDIR}/ --continue  # continue downloading in case it failed last time
+prepare_inputs() {
+  if [[ $FIN =~ ^lumio: ]]; then
+    S3FIN=`echo $FIN | sed 's@lumio:@s3://@'`
+    s3cmd get `echo $S3FIN | sed 's@html.zst@metadata.zst@'` ${OUTDIR}/ --continue  # continue downloading in case it failed last time
+  fi
+}
 
-# --keep-order guarantees that GNU Parallel will collect stdout from tasks and print it to its stdout in the order
-# aligned with the order of input lines
-# --block issues one task per block of input lines of roughly this size, but without breaking lines;
-# making it too small will increase extra costs on script initialization (e.g. weights loading for langid),
-# making it too large will require buffering too much outputs in parallel due to --keep-order requirement.
-echo Running lid in $NJOBS_LID and trafilatura in $NJOBS_TRAF processes
+check_outputs() {
+  # check the number of lines
+  C=`paste <(zstdcat ${OUTDIR}/text.zst|wc -l) <(zstdcat ${OUTDIR}/lang.zst|wc -l) <(zstdcat ${OUTDIR}/metadata.zst|wc -l)`
+  read t l m  <<< "$C"
+  if [ "$t" != "$m" ]; then
+    echo "ERROR: Number of lines mismatch: $t in ${OUTDIR}/text.zst and $m in ${OUTDIR}/metadata.zst"
+    exit 1
+  fi
 
-# rclone occasionally crashes with EOF error when streaming some files from lumio (<1% for bs=10, ~50% for bs=1000)...
-# s3cmd shows warnings about EOF for these files, but retries with success
-#rclone cat $FIN | zstdcat  \
-s3cmd get $S3FIN - | zstdcat  \
-    | parallel --halt now,fail=1 --block $BLOCKSIZE_TRAF -j $NJOBS_TRAF --pipe --keep-order  \
-        "python -m warc2text_runner.stage2.trafilatura.traf --timelimit_perdoc ${TRAF_TIMEOUT}" | tee >(zstd > ${OUTDIR}/text.zst) \
-    | parallel --halt now,fail=1 --block $BLOCKSIZE_LID -j $NJOBS_LID --pipe --keep-order \
-        "python -m warc2text_runner.stage2.fastertext_lid.proto_langid" | zstd > ${OUTDIR}/lang.zst
+  if [ "$l" != "$m" ]; then
+    echo "ERROR: Number of lines mismatch: $l in ${OUTDIR}/lang.zst and $m in ${OUTDIR}/metadata.zst"
+    exit 1
+  fi
 
-# check the number of lines
-C=`paste <(zstdcat ${OUTDIR}/text.zst|wc -l) <(zstdcat ${OUTDIR}/lang.zst|wc -l) <(zstdcat ${OUTDIR}/metadata.zst|wc -l)`
-read t l m  <<< "$C"
-if [ "$t" != "$m" ]; then
-  echo "ERROR: Number of lines mismatch: $t in ${OUTDIR}/text.zst and $m in ${OUTDIR}/metadata.zst"
-  exit 1
-fi
+  echo $t $l $m >${OUTDIR}/.done
+  #rm $FIN
+}
 
-if [ "$l" != "$m" ]; then
-  echo "ERROR: Number of lines mismatch: $l in ${OUTDIR}/lang.zst and $m in ${OUTDIR}/metadata.zst"
-  exit 1
-fi
+stream_html() {
+  # rclone occasionally crashes with EOF error when streaming some files from lumio (<1% for bs=10, ~50% for bs=1000)...
+  # s3cmd shows warnings about EOF for these files, but retries with success
+  if [[ $FIN =~ ^lumio: ]]; then
+    S3FIN=`echo $FIN | sed 's@lumio:@s3://@'`
+    s3cmd get $S3FIN - | zstdcat
+  else
+    rclone cat $FIN | zstdcat
+  fi
+}
 
-echo $t $l $m >${OUTDIR}/.done
-#rm $FIN
+process_html() {
+  NJOBS=`nproc --all`
+  BLOCKSIZE_TRAF=30M  #  more parallel processes than for lid require smaller blocks;
+  TRAF_TIMEOUT=10  # timeout 10s, increased from 0.5s to compensate for adding xml extraction for the 3rd iteration and hopefully get more long good texts
+  BLOCKSIZE_LID=100M  # 0.3s-0.5s to load model, 4.4s to FastText.predict for 10k lines, 28 MB (not random sample!)
+
+  NJOBS=$(($NJOBS - 8))  # leave some threads for rclone, zstdcat, zstd steps in the pipeline
+  NJOBS_LID=$(($NJOBS/10 + 1))
+  NJOBS_TRAF=$(($NJOBS - $NJOBS_LID))
+
+  # --keep-order guarantees that GNU Parallel will collect stdout from tasks and print it to its stdout in the order
+  # aligned with the order of input lines
+  # --block issues one task per block of input lines of roughly this size, but without breaking lines;
+  # making it too small will increase extra costs on script initialization (e.g. weights loading for langid),
+  # making it too large will require buffering too much outputs in parallel due to --keep-order requirement.
+  echo Running lid in $NJOBS_LID and trafilatura in $NJOBS_TRAF processes
+
+  stream_html \
+      | parallel --halt now,fail=1 --block $BLOCKSIZE_TRAF -j $NJOBS_TRAF --pipe --keep-order  \
+          "python -m warc2text_runner.stage2.trafilatura.traf --timelimit_perdoc ${TRAF_TIMEOUT}" | tee >(zstd > ${OUTDIR}/text.zst) \
+      | parallel --halt now,fail=1 --block $BLOCKSIZE_LID -j $NJOBS_LID --pipe --keep-order \
+          "python -m warc2text_runner.stage2.fastertext_lid.proto_langid" | zstd > ${OUTDIR}/lang.zst
+}
+
+time prepare_inputs
+time process_html
+time check_outputs
--- src/warc2text_runner/stage2/stage2local_batch.sh
+++ src/warc2text_runner/stage2/stage2local_batch.sh
@@ -12,7 +12,7 @@ for x in ${@:2}; do
 #    OUTDIR=$(dirname $x)
     OUTDIR=${BASEOUTDIR}/`echo $x | sed -r "s@.*lumio:(.*)/html.zst@\1@"`
     echo "Processing $x, writing to ${OUTDIR}" 1>&2  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
-    time stage2local.sh $x ${OUTDIR} || { rc="$?"; echo "ERROR: stage2local.sh exited with code $rc while processing $x" 1>&2; }  # in bash && and || have the same priority
+    stage2local.sh $x ${OUTDIR} || { rc="$?"; echo "ERROR: stage2local.sh exited with code $rc while processing $x" 1>&2; }  # in bash && and || have the same priority
     echo "stage2local_batch.sh planned exit code:" $rc 1>&2
 done
 

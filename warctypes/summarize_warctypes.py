ff224ae283e3d4c9740a9aa909d6af927ad51822
nvanva <ezhick179@gmail.com>
2025-04-24 15:09:17 +0200

    content and record types statistics for warc files

--- warctypes/summarize_warctypes.py
+++ warctypes/summarize_warctypes.py
@@ -6,7 +6,8 @@ def summarize_warctypes(inputdir, t):
     df = pd.concat([pd.read_csv(f, sep='\t') for f in Path(inputdir).glob(f'*{t}.tsv')])
     df['content-length'] = pd.to_numeric(df['content-length'], errors='coerce')
     df = df.dropna()
-    mdf = df.groupby('content-type').agg({'content-length':'sum'}).sort_values(by='content-length')//2**30
+    mdf = df.groupby('content-type').agg({'content-length':'sum'}).sort_values(by='content-length')/2**30
+    mdf.to_csv(Path(inputdir)/f'total-{t}',sep='\t')
     print(mdf.tail(25))
     print('\nTotal size accounted for:', mdf.sum().iloc[0])
     print((mdf/mdf.sum()).tail(25))

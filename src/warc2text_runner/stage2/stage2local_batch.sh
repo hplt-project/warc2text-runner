c81348e416e4c51cb9ca28cb2bc27b199b8ac343
nvanva <ezhick179@gmail.com>
2025-04-24 02:42:16 +0200

    stage2: staging

--- src/warc2text_runner/stage2/stage2local_batch.sh
+++ src/warc2text_runner/stage2/stage2local_batch.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 BASEOUTDIR=$1
 
+getoutdir() {
+#    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r 's!.*(/[^/]+/[^/]+/)[^/]+!\1!'`  # BASEOUTDIR/crawl/batch_id/
+#    OUTDIR=$(dirname $x)
+    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r "s@[^:]+:(.*)/html.zst@\1@"`
+    echo $OUTDIR
+}
+
+process() {
+    local x=$1
+    local OUTDIR=$2
+    echo "$(date) Processing $x, writing to ${OUTDIR}"  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
+    stage2local.sh $x ${OUTDIR}
+    c="$?"
+    echo "$(date) stage2local_batch.sh exit code: $c"
+    return $c
+}
+
+stage() {
+    local x=$1
+    local OUTDIR=$2
+    s3cmd get "$x" "$OUTDIR" --continue
+}
+
+clean() {
+    local OUTDIR=$1
+    rm -f "$OUTDIR"/html.zst
+}
+
 echo "$(date) stage2local_batch.sh: running at `hostname`"
 echo "$(date) stage2local_batch.sh: processing ${@:2}, writing to $BASEOUTDIR"
 echo -n "Total size in GB: "
 echo "${@:2}"|tr ' ' '\n' | xargs -n1 rclone lsjson|jq -c '.[]|.Size' | awk '{sum+=$1} END {print sum/2**30}'
 
 rc=0
-for x in ${@:2}; do 
-#    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r 's!.*(/[^/]+/[^/]+/)[^/]+!\1!'`  # BASEOUTDIR/crawl/batch_id/
-#    OUTDIR=$(dirname $x)
-    OUTDIR=${BASEOUTDIR}/`echo $x | sed -r "s@[^:]+:(.*)/html.zst@\1@"`
-    echo "$(date) Processing $x, writing to ${OUTDIR}"  # forward everything to stderr to interleave correctly with error messages from stage2local.sh
-    stage2local.sh $x ${OUTDIR}
-    c="$?"
-    echo "$(date) stage2local_batch.sh exit code: $c"
-    if [ "$c" -ne 0 ]; then
-       rc="$c"
-       echo "$(date) stage2local_batch.sh: ERROR: processing $x, writing to ${OUTDIR} finished with code $c" 1>&2
-    fi
+current="$2"
+for next in "${@:3}"; do
+    stage "$next" "$(getoutdir "$next")" &
+    staging_job=$!
+    process "$current" "$(getoutdir "$current")" && clean "$(getoutdir "$current")" || rc="$?"
+    wait $staging_job && current="$(getoutdir "$next")/html.zst" || current=$next
 done
+process "$current" || rc="$?"
 
 if [ "$rc" -eq 0 ]; then
   echo "$(date) stage2local_batch.sh: all files processed successfully"
